023971f0a88cb4205d6e1a42ccd0d3b48fe32869
lumakos <lumakos23@gmail.com>
2017-04-27 22:16:59 +0300

    first

---
+++ js/slider.js
@@ -0,0 +1,39 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+        $('.carousel').on('slid.bs.carousel', function () {
+
+          var carouselData = $(this).data('bs.carousel');
+          var currentIndex = carouselData.getItemIndex(carouselData.$element.find('.item.active'));
+          var total = carouselData.$items.length;
+
+          // Now display this wherever you want
+          var text = (currentIndex + 1) + "/" + total;
+          $('#slidetext').text(text);
+
+        });
+
+        $(".right").on('mouseenter', function (e) {
+            var currentActive = $('.carousel-inner').find('.item.active').next();
+            var next = currentActive.find('.carousel-caption').html();
+            if (typeof next != 'undefined') {
+              $('.next-btn').after(next);
+            }
+        }).on('mouseleave', function (e) {
+            $('.right p').remove();
+        });
+
+        $(".left").on('mouseenter', function (e) {
+            var preActive = $('.carousel-inner').find('.item.active').prev();
+            var previous = preActive.find('.carousel-caption').html();
+            if (typeof previous != 'undefined') {
+              $('.prev-btn').after(previous);
+            }
+        }).on('mouseleave', function (e) {
+          $('.left p').remove();
+        });
+
+    });
+
+}(jQuery));
